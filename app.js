1bca44e61dd7cac3ae2fed2208fd08f43a515d30
khititach <khititachk58@email.nu.ac.th>
2019-05-07 23:28:45 +0700

    edit name floder and file

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ var app = express();
 var path = require('path');
 
 app.use('/',express.static('Login'))
-app.use('/fo',express.static('FO'))
+app.use('/officer',express.static('Officer'))
 app.use('/student',express.static('Student'))
 app.use('/store',express.static('Store'))
 app.get('/',function(req, res){
     res.sendFile(path.join(__dirname + '/Login/login.html'));
 });
 app.get('/fo',function(req,res){
-    res.sendFile(__dirname + '/FO/fo.html');
+    res.sendFile(__dirname + '/Officer/officer.html');
 })
 app.get('/fo/reg',function(req,res){
-    res.sendFile(__dirname + '/FO/p2_register.html');
+    res.sendFile(__dirname + '/Officer/officer_register_page.html');
 })
 app.get('/fo/topup',function(req,res){
-    res.sendFile(__dirname + '/FO/p3_topup.html');
+    res.sendFile(__dirname + '/Officer/officer_topup_page.html');
 })
 app.get('/student',function(req,res){
     res.sendFile(__dirname + '/Student/student.html');
@@ -25,6 +25,6 @@ app.get('/store',function(req,res){
     res.sendFile(__dirname + '/Store/store.html');
 })
 app.get('/store/income',function(req,res){
-    res.sendFile(__dirname + '/Store/p2_income.html');
+    res.sendFile(__dirname + '/Store/store_income.html');
 })
 app.listen(8002)
